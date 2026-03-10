d5d7a0ba9ef5880e68a6a3754d0acfb242535ab0
Jackshalom667 <jackshalom34@gmail.com>
2026-03-10 12:57:41 +0100

    Initial CI/CD project

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django_password',
+        'HOST':'db',
+        'PORT': '5432',
     }
 }
 
